d702c8cb9b638545d1412d9558445d860ab321ae
AndreasRG <andreas@revesz-gudmann.dk>
2024-10-16 15:06:51 +0200

    Snapshot 16/10-24 15:06 Beginning of adding different pages. Updated and added various HTML, js and css files

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-//JAVASCRIPT FOR index.html
+//BASAL JAVASCRIPT FOR PERSONAL PORTFOLIO
 
 /*Light/dark mode switch start:*/
 //Selecting different elements in HTML:
-const togglePageModeButton = document.querySelector("nav ul li button");
+const togglePageModeButton = document.querySelector(".lightDarkModeBtn");
 const bodySelector = document.querySelector("body");
 const profileImageSelector = document.querySelector("#introduction div img");
 const allATagSelector = document.querySelectorAll("body a");
+const allNavButtonSelector = document.querySelectorAll(".navBtn")
 let booleanMode = 0;
 
-// Function to change color of all <a> tags
+// Function to change color of all <a> tags:
 const changeATagColor = (color) => {
     allATagSelector.forEach(aTag => {
         aTag.style.color = color;
+    });
+};
+
+// Function to change color of all nav buttons:
+const changeNavButtonColor = (color) => {
+    allNavButtonSelector.forEach(navButton => {
+        navButton.style.color = color;
     });
 };
 
@@ -24,6 +32,7 @@ togglePageModeButton.addEventListener("click", () => {
         bodySelector.style.backgroundColor = "black";
         profileImageSelector.style.borderColor = "white";
         togglePageModeButton.style.backgroundImage = `url("lightModeIcon.png")`;
+        changeNavButtonColor("white");
         changeATagColor("white");
     } else {
         booleanMode = 0;
@@ -31,6 +40,7 @@ togglePageModeButton.addEventListener("click", () => {
         bodySelector.style.backgroundColor = "white";
         profileImageSelector.style.borderColor = "grey";
         togglePageModeButton.style.backgroundImage = `url("darkModeIcon.png")`;
+        changeNavButtonColor("black");
         changeATagColor("black");
     }
 })
@@ -50,4 +60,52 @@ togglePageModeButton.addEventListener("mouseout", () => {
         togglePageModeButton.style.backgroundImage = `url("darkModeIcon.png")`;
     }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const togglePageModeButton = document.querySelector(".lightDarkModeBtn");
+    const bodySelector = document.querySelector("body");
+    const profileImageSelector = document.querySelector("#introduction div img");
+    const allATagSelector = document.querySelectorAll("body a");
+    const allNavButtonSelector = document.querySelectorAll(".navBtn");
+    let booleanMode = 0;
+
+    const changeATagColor = (color) => {
+        allATagSelector.forEach(aTag => {
+            aTag.style.color = color;
+        });
+    };
+
+    const changeNavButtonColor = (color) => {
+        allNavButtonSelector.forEach(navButton => {
+            navButton.style.color = color;
+        });
+    };
+
+    if (togglePageModeButton) {
+        togglePageModeButton.addEventListener("click", () => {
+            if (booleanMode === 0) {
+                booleanMode = 1;
+                if (bodySelector) {
+                    bodySelector.style.transition = "0.2s";
+                    bodySelector.style.color = "white";
+                    bodySelector.style.backgroundColor = "black";
+                }
+                if (profileImageSelector) profileImageSelector.style.borderColor = "white";
+                togglePageModeButton.style.backgroundImage = `url("lightModeIcon.png")`;
+                changeNavButtonColor("white");
+                changeATagColor("white");
+            } else {
+                booleanMode = 0;
+                if (bodySelector) {
+                    bodySelector.style.color = "black";
+                    bodySelector.style.backgroundColor = "white";
+                }
+                if (profileImageSelector) profileImageSelector.style.borderColor = "grey";
+                togglePageModeButton.style.backgroundImage = `url("darkModeIcon.png")`;
+                changeNavButtonColor("black");
+                changeATagColor("black");
+            }
+        });
+    }
+});
 /*Light/dark mode switch end:*/
