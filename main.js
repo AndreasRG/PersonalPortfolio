1797bbdb97321a678aba21fb261ffee53e7c47e0
AndreasRG <andreas@revesz-gudmann.dk>
2025-01-06 20:53:03 +0100

    Snapshot 06/01-2025 20:52 Updating Codebook as I do notes

--- main.js
+++ main.js
@@ -111,9 +111,18 @@ document.addEventListener("DOMContentLoaded", () => {
 /*Light/dark mode switch end:*/
 
 
+fetch('https://yesno.wtf/api/')
+    .then(response => response.json())
+    .then(yesOrNoData => {
+        console.log(yesOrNoData);
+        // HERE IS WHERE YOU WRITE YOUR CODE!!!!!!!!
+    });
 
-
-
+fetch('URL')
+    .then(response => response.json())
+    .then(dataName => {
+        console.log(dataName);
+    });
 
 
 /*Data test start:
@@ -145,4 +154,3 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 /*Data test end*/
-
